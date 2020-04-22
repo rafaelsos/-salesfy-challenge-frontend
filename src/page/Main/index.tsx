4aec6f5e01992fb78ce39f7dc0c458bbc6c767c2
Sosnowski0304 <rafael.sosnowski@gmail.com>
2020-04-21 21:50:31 -0300

    estilizando page Main

--- src/page/Main/index.tsx
+++ src/page/Main/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import logoNubank from '../../assets/Nubank_Logo.png';
 
-import { Container, Header } from './styles';
+import { Container, Header, Card } from './styles';
 
 export default function Main() {
   return (
@@ -11,6 +11,15 @@ export default function Main() {
         <img src={logoNubank} alt="nubankLogo" />
         <h1>Salesfy</h1>
       </Header>
+      <Card>
+        <input type="text" placeholder="Digite o número aqui..." />
+        <span>Número Traduzido 9999999</span>
+        <strong>
+          nine hundred ninety-nine million nine hundred ninety-nine thousand
+          nine hundred ninety-nine
+        </strong>
+        <button type="button">Traduzir</button>
+      </Card>
     </Container>
   );
 }
--- src/page/Main/styles.ts
+++ src/page/Main/styles.ts
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { darken } from 'polished';
 
 export const Container = styled.div`
   display: flex;
@@ -23,5 +24,65 @@ export const Header = styled.div`
     color: #fff;
     font-weight: bold;
     margin-left: 8px;
+  }
+`;
+
+export const Card = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  border-radius: 2px;
+  padding: 20px;
+  margin-top: 10px;
+
+  width: 400px;
+  height: 200px;
+  background: #fff;
+
+  input {
+    font-size: 24px;
+    font-weight: bold;
+    padding: 10px;
+    border-radius: 4px;
+    border: none;
+    background: #f0f0f0;
+    text-align: center;
+    width: 250px;
+    height: 40px;
+
+    &::placeholder {
+      color: #dedede;
+      font-size: 14px;
+    }
+  }
+
+  span {
+    font-size: 14px;
+    font-weight: bold;
+    color: #565656;
+    margin-bottom: 10px;
+  }
+
+  strong {
+    height: 60px;
+  }
+
+  button {
+    font-size: 20px;
+    font-weight: bold;
+    padding: 10px;
+    border-radius: 4px;
+    border: none;
+    background: rgb(138, 5, 170);
+    color: #fff;
+    text-align: center;
+    width: 250px;
+    height: 40px;
+    margin-top: auto;
+    transition: background 0.2s;
+
+    &:hover {
+      background: ${darken(0.03, 'rgb(138, 5, 170)')};
+    }
   }
 `;
